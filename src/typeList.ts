143c7937877e812c0e2febb68d4a05ff7e0abbec
kotomi-san <47980784+ko-to-mi-san@users.noreply.github.com>
2022-03-24 09:55:48 +0900

    typescript型

---
+++ src/typeList.ts
@@ -0,0 +1,24 @@
+let bool: boolean = true;
+
+let num: number = 0;
+
+let str: string = "a";
+
+let arr1: Array<number> = [0, 1, 2];
+
+let arr2: number[] = [0, 1, 2];
+
+let tuple: [number, string] = [0, "A"];
+
+let any1: any = false;
+
+const func1 = (): void => {
+  const test = "TEST";
+};
+
+let null1: null = null;
+
+let undefined1: undefined = undefined;
+
+let obj1: object = {};
+let obj2: { id: number; name: string } = { id: 1, name: "aaa" };
