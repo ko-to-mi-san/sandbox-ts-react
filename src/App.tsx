1dda6cf7a257da251f118ac63b972472e480c005
kotomi-san <47980784+ko-to-mi-san@users.noreply.github.com>
2022-04-08 09:56:38 +0900

    user情報表示

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,17 @@
-import axios from "axios";
-import { useState } from "react";
 import "./styles.css";
-import { Todo } from "./Todo";
-import { TodoType } from "./types/todo";
-import { Text } from "./Text";
-import { UserProfile } from "./UserProfile";
+import { UserCard } from "./components/UserCard";
 
 const user = {
+  id: 1,
   name: "ぴーつん",
-  hobbies: ["漫画読む", "ランニング"]
+  email: "123@example.com",
+  address: "地球"
 };
 
 export default function App() {
-  const [todos, setTodos] = useState<Array<TodoType>>([]);
-
-  const onClickFetchData = () => {
-    axios
-      .get<Array<TodoType>>("https://jsonplaceholder.typicode.com/todos")
-      .then((res) => {
-        setTodos(res.data);
-      });
-  };
   return (
     <div className="App">
-      <UserProfile user={user} />
-      <Text color="red" fontSize="18px" />
-      <button onClick={onClickFetchData}>データ取得</button>
-      {todos.map((todo) => (
-        <Todo
-          key={todo.id}
-          title={todo.title}
-          userId={todo.userId}
-          completed={todo.completed}
-        />
-      ))}
+      <UserCard user={user} />
     </div>
   );
 }
